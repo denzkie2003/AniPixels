752f8ea0621b44a5b3c8f1ee8956345ed06d94c6
denzkie2003 <83795556+denzkie2003@users.noreply.github.com>
2021-05-07 09:17:06 +0800

    update searchEngine.js

--- searchEngine.js
+++ searchEngine.js
@@ -1,1 +1,33 @@
 
+
+
+function searchAnime() {
+
+
+
+  var input, filter, ol, li, a, i, txtValue, divShow;
+  input = document.getElementById('search');
+  filter = input.value.toUpperCase();
+  
+  divShow = document.getElementById('notFound');
+  
+  ol = document.getElementById("resultFound");
+  li = ol.getElementsByTagName('li');
+ 
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      
+      li[i].style.display = "block";
+      
+    } else {
+      
+      li[i].style.display = "none";
+    
+    }
+   
+      
+  }
+}
