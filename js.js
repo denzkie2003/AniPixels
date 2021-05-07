08e03479566d5b9bd77feabf472ba99ef11f95c9
denzkie2003 <83795556+denzkie2003@users.noreply.github.com>
2021-05-07 09:16:44 +0800

    update js.js

--- js.js
+++ js.js
@@ -1,1 +1,26 @@
 
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "60%";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+document.getElementById('s').addEventListener('click',open_close);
+
+var menuState = 0;
+function open_close() {
+  if(menuState === 0){
+     menuState = 1;
+     document.getElementById("search").style.width = "70%";
+     document.getElementById('searchDiv').style.width = '100%';
+  }
+  else {
+     menuState = 0;
+     document.getElementById("search").style.width = "0";
+     document.getElementById('searchDiv').style.width = '0';
+  }
+ 
+}
+
